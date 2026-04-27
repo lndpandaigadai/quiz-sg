e925530b4283c89fae65d8578d889170e274c8bd
lndpandaigadai <andri.nugroho@pintarnya.com>
2026-04-27 16:52:08 +0700

    Update analisis.py

--- analisis.py
+++ analisis.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.set_page_config(page_title="Dashboard Misgrading", layout="wide")
 st.title("📊 Dashboard Misgrading - Region 2")
 
-file_path = "Analisis need coaching rn 2egio.xlsx"
+file_path = "Analisis need coaching rn 2egio.txt"
 
 # ==============================
 # LOAD DATA
